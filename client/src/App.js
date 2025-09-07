03b02a33be847b6babac32c76a5b531e87158874
Ansh <anshkant090@gmail.com>
2025-09-08 00:25:30 +0530

    guest login part added

--- client/src/App.js
+++ client/src/App.js
@@ -8,51 +8,61 @@ import Signup from "./pages/Signup";
 import { useDispatch } from "react-redux";
 import Cookies from "js-cookie";
 import { authActions } from "./store/authSlice";
-import CheckAuth from './utils/CheckAuth'
+import CheckAuth from "./utils/CheckAuth";
 
 function App() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const token = Cookies.get("auth-token");
 
-  const getUserDetails = async () => {
-      const response = await fetch(
-        `${process.env.REACT_APP_API_URL}/auth/getuser`,
-        {
-          method: "GET",
-          headers: {
-            "content-type": "application/json",
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-  
-      if (response.ok) {
-        const responseData = await response.json();
-        dispatch(authActions.loginSuccess({ user: responseData.user }));
-      }
-      else {
-        navigate('/login');
-        return ;
-      }
-    }
-  
   useEffect(() => {
-    if(!token) {
-      // if token is not present, then redirect to login page
-      navigate('/login');
-      return ;
+    const getUserDetails = async () => {
+      try {
+        const response = await fetch(
+          `${process.env.REACT_APP_API_URL}/auth/getuser`,
+          {
+            method: "GET",
+            headers: {
+              "content-type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        if (response.ok) {
+          const responseData = await response.json();
+          dispatch(authActions.loginSuccess({ user: responseData.user }));
+        } else {
+          Cookies.remove("auth-token");
+          navigate("/login");
+        }
+      } catch (error) {
+        console.error("Error fetching user details:", error);
+        Cookies.remove("auth-token");
+        navigate("/login");
+      }
+    };
+
+    if (!token) {
+      navigate("/login");
+      return;
     }
 
-    // if token is present, then validate it.
-    getUserDetails()
-  },[]);
+    getUserDetails();
+  }, [token, navigate, dispatch]);
 
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<CheckAuth><Home /></CheckAuth>} />
+        <Route
+          path="/"
+          element={
+            <CheckAuth>
+              <Home />
+            </CheckAuth>
+          }
+        />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -8,111 +8,131 @@ import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import {useDispatch } from 'react-redux'
-import {useNavigate} from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { authActions } from '../store/authSlice';
 import Cookies from 'js-cookie';
-import {Link} from "react-router-dom"
+import { Link } from 'react-router-dom';
 
 export default function Login() {
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-  const loginUser = async (userData) =>{  
-      const response = await fetch(`${process.env.REACT_APP_API_URL}/auth/login`, {
-        method: "POST",
-        body: JSON.stringify(userData),
-        headers: {
-          "content-type": "application/json",
-        },
-      });
-  
-      const responseData = await response.json();
-      
-      if(!response.ok) {
-        alert("Not able to login, Please Enter valid Credentials");
-        console.log('could not login')
-        return ;
-      }
-  
-      // else set the auth token.
-      Cookies.set('auth-token', responseData.authToken);
-      dispatch(authActions.loginSuccess({user: responseData.user}));
-      navigate('/');
+  const loginUser = async (userData) => {
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/auth/login`, {
+      method: 'POST',
+      body: JSON.stringify(userData),
+      headers: {
+        'content-type': 'application/json',
+      },
+    });
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      alert('Not able to login, Please Enter valid Credentials');
+      console.log('could not login');
+      return;
     }
 
+    // set the auth token.
+    Cookies.set('auth-token', responseData.authToken);
+    dispatch(authActions.loginSuccess({ user: responseData.user }));
+    navigate('/');
+  };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
- 
+
     const formFields = {
       email: data.get('email'),
       password: data.get('password'),
-    }
+    };
 
     await loginUser(formFields);
   };
 
+  // Guest login handler
+  const handleGuestLogin = async () => {
+    const guestCredentials = {
+      email: 'guest@example.com',
+      password: 'guest123',
+    };
+    await loginUser(guestCredentials);
+  };
+
   return (
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-         
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign In
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Typography variant="body2" component="span" sx={{ mr: 1 }}>
-                    Don't have an account?
-                  </Typography>
-                <Link to="/signup" style={{ textDecoration: "none" }}>
-                  Sign Up
-                </Link>
-              </Grid>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+
+          {/* Normal Login */}
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 1 }}
+          >
+            Sign In
+          </Button>
+
+          {/* Guest Login */}
+          <Button
+            onClick={handleGuestLogin}
+            fullWidth
+            variant="outlined"
+            sx={{ mb: 2 }}
+          >
+            Continue as Guest
+          </Button>
+
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Typography variant="body2" component="span" sx={{ mr: 1 }}>
+                Don't have an account?
+              </Typography>
+              <Link to="/signup" style={{ textDecoration: 'none' }}>
+                Sign Up
+              </Link>
             </Grid>
-          </Box>
+          </Grid>
         </Box>
-      </Container>
+      </Box>
+    </Container>
   );
 }
